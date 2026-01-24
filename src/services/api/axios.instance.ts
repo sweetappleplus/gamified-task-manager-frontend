bfa9feaf3ca6fa6af6c192d6d5b4c66650d402a2
sweetappleplus <sweetappleplus@gmail.com>
2026-01-24 02:16:41 +0200

    chore(constants): add API_URL constant and update exports

--- src/services/api/axios.instance.ts
+++ src/services/api/axios.instance.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
-
-export const API_URL = process.env["REACT_APP_API_URL"];
-
-if (!API_URL) {
-  throw new Error("REACT_APP_API_URL environment variable is not defined");
-}
+import { API_URL } from "@/constants";
 
 export const api = axios.create({
   baseURL: API_URL,
