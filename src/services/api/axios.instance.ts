dcae579df77ae2caf2f18507064e7fa930ae348f
sweetappleplus <sweetappleplus@gmail.com>
2026-01-24 12:19:42 +0200

    fix(api): ensure API_URL is set in environment variables

--- src/services/api/axios.instance.ts
+++ src/services/api/axios.instance.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { API_URL } from "consts";
 
+if (!API_URL) {
+  throw new Error("API_URL is not set in the environment variables");
+}
+
 export const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
