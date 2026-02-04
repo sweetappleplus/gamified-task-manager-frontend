52c9c674587c0d504970e0fb3c0fc8b9284f0af6
sweetappleplus <sweetappleplus@gmail.com>
2026-02-04 16:05:34 +0200

    feat(icons): add new SVG icons and refactor icon types

---
+++ src/components/atoms/Icon/icons/index.ts
@@ -0,0 +1,169 @@
+import { ReactComponent as ArrowRight } from "./arrow-right.svg";
+import { ReactComponent as ArrowLeft } from "./arrow-left.svg";
+import { ReactComponent as ArrowUp } from "./arrow-up.svg";
+import { ReactComponent as ArrowDown } from "./arrow-down.svg";
+
+import { ReactComponent as ArrowRightDoubleSharp } from "./arrow-right-double-sharp.svg";
+import { ReactComponent as ArrowLeftDoubleSharp } from "./arrow-left-double-sharp.svg";
+
+import { ReactComponent as ChevronRight } from "./chevron-right.svg";
+import { ReactComponent as ChevronLeft } from "./chevron-left.svg";
+import { ReactComponent as ChevronUp } from "./chevron-up.svg";
+import { ReactComponent as ChevronDown } from "./chevron-down.svg";
+
+import { ReactComponent as BookmarkEmpty } from "./bookmark-empty.svg";
+import { ReactComponent as BookmarkFill } from "./bookmark-fill.svg";
+import { ReactComponent as Bank } from "./bank.svg";
+import { ReactComponent as Bell } from "./bell.svg";
+import { ReactComponent as Cash } from "./cash.svg";
+import { ReactComponent as Calendar } from "./calendar.svg";
+import { ReactComponent as Chart } from "./chart.svg";
+import { ReactComponent as ClockCircle } from "./clock-circle.svg";
+import { ReactComponent as ClockRunning } from "./clock-running.svg";
+import { ReactComponent as DocumentText } from "./document-text.svg";
+import { ReactComponent as Dots } from "./dots.svg";
+import { ReactComponent as Down } from "./down.svg";
+import { ReactComponent as Edit } from "./edit.svg";
+import { ReactComponent as Empty } from "./empty.svg";
+import { ReactComponent as EyeSlash } from "./eye-slash.svg";
+import { ReactComponent as Eye } from "./eye.svg";
+import { ReactComponent as FileAdd } from "./file-add.svg";
+import { ReactComponent as File } from "./file.svg";
+import { ReactComponent as Gift } from "./gift.svg";
+import { ReactComponent as Home } from "./home.svg";
+import { ReactComponent as InformationCircle } from "./information-circle.svg";
+import { ReactComponent as Link } from "./link.svg";
+import { ReactComponent as Lists } from "./lists.svg";
+import { ReactComponent as Message } from "./message.svg";
+import { ReactComponent as Minus } from "./minus.svg";
+import { ReactComponent as PlusCircle } from "./plus-circle.svg";
+import { ReactComponent as Plus } from "./plus.svg";
+import { ReactComponent as Prize } from "./prize.svg";
+import { ReactComponent as Rating } from "./rating.svg";
+import { ReactComponent as Rocket } from "./rocket.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as Star } from "./star.svg";
+import { ReactComponent as TickCircle } from "./tick-circle.svg";
+import { ReactComponent as Tick } from "./tick.svg";
+import { ReactComponent as Trash } from "./trash.svg";
+import { ReactComponent as Up } from "./up.svg";
+import { ReactComponent as Upload } from "./upload.svg";
+import { ReactComponent as UserSquare } from "./user-square.svg";
+import { ReactComponent as WalletClose } from "./wallet-close.svg";
+import { ReactComponent as WalletOpen } from "./wallet-open.svg";
+import { ReactComponent as XCircle } from "./x-circle.svg";
+import { ReactComponent as X } from "./x.svg";
+
+import { ReactComponent as DiamondColored } from "./diamond-colored.svg";
+import { ReactComponent as StarSilverColored } from "./star-silver-colored.svg";
+import { ReactComponent as StarGoldColored } from "./star-gold-colored.svg";
+import { ReactComponent as FireColored } from "./fire-colored.svg";
+import { ReactComponent as FireDisabledColored } from "./fire-disabled-colored.svg";
+import { ReactComponent as TickColored } from "./tick-colored.svg";
+import { ReactComponent as AttentionColored } from "./attention-colored.svg";
+import { ReactComponent as UsdtColored } from "./usdt-colored.svg";
+import { ReactComponent as ChestColored } from "./chest-colored.svg";
+import { ReactComponent as TelegramSquareColored } from "./telegram-square-colored.svg";
+import { ReactComponent as WhatsappSquareColored } from "./whatsapp-square-colored.svg";
+import { ReactComponent as EmailSquareColored } from "./email-square-colored.svg";
+import { ReactComponent as GoogleColored } from "./google-colored.svg";
+import { ReactComponent as MicrosoftColored } from "./microsoft-colored.svg";
+import { ReactComponent as WhatsappColored } from "./whatsapp-colored.svg";
+import { ReactComponent as TelegramColored } from "./telegram-colored.svg";
+import { ReactComponent as DiscordColored } from "./discord-colored.svg";
+import { ReactComponent as FacebookColored } from "./facebook-colored.svg";
+import { ReactComponent as LinkedinColored } from "./linkedin-colored.svg";
+import { ReactComponent as MirColored } from "./mir-colored.svg";
+import { ReactComponent as VisaColored } from "./visa-colored.svg";
+import { ReactComponent as MastercardColored } from "./mastercard-colored.svg";
+import { ReactComponent as UnionpayColored } from "./unionpay-colored.svg";
+import { ReactComponent as JcbColored } from "./jcb-colored.svg";
+import { ReactComponent as DinersClubColored } from "./diners-club-colored.svg";
+import { ReactComponent as AmericanExpressColored } from "./american-express-colored.svg";
+
+export const icons = {
+  "arrow-right": ArrowRight,
+  "arrow-left": ArrowLeft,
+  "arrow-up": ArrowUp,
+  "arrow-down": ArrowDown,
+
+  "arrow-right-double-sharp": ArrowRightDoubleSharp,
+  "arrow-left-double-sharp": ArrowLeftDoubleSharp,
+
+  "chevron-right": ChevronRight,
+  "chevron-left": ChevronLeft,
+  "chevron-up": ChevronUp,
+  "chevron-down": ChevronDown,
+
+  "bookmark-empty": BookmarkEmpty,
+  "bookmark-fill": BookmarkFill,
+  bank: Bank,
+  bell: Bell,
+  cash: Cash,
+  calendar: Calendar,
+  chart: Chart,
+  "clock-circle": ClockCircle,
+  "clock-running": ClockRunning,
+  "document-text": DocumentText,
+  dots: Dots,
+  down: Down,
+  edit: Edit,
+  empty: Empty,
+  "eye-slash": EyeSlash,
+  eye: Eye,
+  "file-add": FileAdd,
+  file: File,
+  gift: Gift,
+  home: Home,
+  "information-circle": InformationCircle,
+  link: Link,
+  lists: Lists,
+  message: Message,
+  minus: Minus,
+  "plus-circle": PlusCircle,
+  plus: Plus,
+  prize: Prize,
+  rating: Rating,
+  rocket: Rocket,
+  search: Search,
+  star: Star,
+  "tick-circle": TickCircle,
+  tick: Tick,
+  trash: Trash,
+  up: Up,
+  upload: Upload,
+  "user-square": UserSquare,
+  "wallet-close": WalletClose,
+  "wallet-open": WalletOpen,
+  "x-circle": XCircle,
+  x: X,
+
+  "diamond-colored": DiamondColored,
+  "star-silver-colored": StarSilverColored,
+  "star-gold-colored": StarGoldColored,
+  "fire-colored": FireColored,
+  "fire-disabled-colored": FireDisabledColored,
+  "tick-colored": TickColored,
+  "attention-colored": AttentionColored,
+  "usdt-colored": UsdtColored,
+  "chest-colored": ChestColored,
+  "telegram-square-colored": TelegramSquareColored,
+  "whatsapp-square-colored": WhatsappSquareColored,
+  "email-square-colored": EmailSquareColored,
+  "google-colored": GoogleColored,
+  "microsoft-colored": MicrosoftColored,
+  "whatsapp-colored": WhatsappColored,
+  "telegram-colored": TelegramColored,
+  "discord-colored": DiscordColored,
+  "facebook-colored": FacebookColored,
+  "linkedin-colored": LinkedinColored,
+  "mir-colored": MirColored,
+  "visa-colored": VisaColored,
+  "mastercard-colored": MastercardColored,
+  "unionpay-colored": UnionpayColored,
+  "jcb-colored": JcbColored,
+  "diners-club-colored": DinersClubColored,
+  "american-express-colored": AmericanExpressColored,
+};
+
+export type IconName = keyof typeof icons;
