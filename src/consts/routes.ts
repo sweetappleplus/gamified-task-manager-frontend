517940a7f2e16ff50816c30205765693a4f8e39b
sweetappleplus <sweetappleplus@gmail.com>
2026-02-05 14:58:38 +0200

    refactor(routes): add access control to route configurations

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -1,24 +1,22 @@
-import { ComponentType } from "react";
+import { RouteConfig, ROUTE_ACCESS } from "types";
 import UIKitPage from "pages/UIKit";
 import LoginPage from "pages/Login";
 import DashboardPage from "pages/Dashboard";
-
-export interface RouteConfig {
-  path: string;
-  component: ComponentType;
-}
 
 export const ROUTES: Record<string, RouteConfig> = {
   UI_KIT: {
     path: "/uikit",
     component: UIKitPage,
+    access: ROUTE_ACCESS.PUBLIC,
   },
   LOGIN: {
     path: "/login",
     component: LoginPage,
+    access: ROUTE_ACCESS.PUBLIC,
   },
   DASHBOARD: {
     path: "/dashboard",
     component: DashboardPage,
+    access: ROUTE_ACCESS.PROTECTED,
   },
 } as const;
