40b6c698171055e8f9d55645873a2ae550f7277e
sweetappleplus <sweetappleplus@gmail.com>
2026-02-05 13:26:43 +0200

    refactor(components): move getInitials function to utils

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 import { Avatar as MuiAvatar } from "@mui/material";
 import { AvatarProps } from "./Avatar.types";
-
-const getInitials = (name?: string, email?: string): string => {
-  if (name) {
-    const words = name.trim().split(/\s+/);
-    if (words.length >= 2) {
-      return `${words[0][0]}${words[1][0]}`.toUpperCase();
-    }
-    return name.slice(0, 2).toUpperCase();
-  }
-
-  if (email) {
-    const localPart = email.split("@")[0];
-    return localPart.slice(0, 2).toUpperCase();
-  }
-
-  return "";
-};
+import { getInitials } from "utils";
 
 export const Avatar: React.FC<AvatarProps> = ({
   size = 48,
