db7f597ed88f0f62f78e93ec4d697cd444a306a7
sweetappleplus <sweetappleplus@gmail.com>
2026-02-06 14:08:20 +0200

    refactor(routes): simplify PublicRoute component logic

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,13 +1,5 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "features/auth";
 import { PublicRouteProps } from "types";
 
 export const PublicRoute = ({ children }: PublicRouteProps) => {
-  const { isAuthenticated } = useAuth();
-
-  if (isAuthenticated) {
-    return <Navigate to="/dashboard" replace />;
-  }
-
   return <>{children}</>;
 };
