013daba8c6343a07a7ec9f342e63e2b167aee250
sweetappleplus <sweetappleplus@gmail.com>
2026-02-09 12:15:51 +0200

    fix(layouts): pass items prop to FooterNavLinks in WorkerFooter

--- src/components/layouts/WorkerFooter/WorkerFooter.tsx
+++ src/components/layouts/WorkerFooter/WorkerFooter.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { FooterNavLinks } from "components";
+import { WORKER_FOOTER_NAV_ITEMS } from "consts";
 import { WorkerFooterProps } from "./WorkerFooter.types";
 
 export const WorkerFooter: React.FC<WorkerFooterProps> = ({ activeRoute }) => {
-  return <FooterNavLinks activeRoute={activeRoute} />;
+  return (
+    <FooterNavLinks items={WORKER_FOOTER_NAV_ITEMS} activeRoute={activeRoute} />
+  );
 };
