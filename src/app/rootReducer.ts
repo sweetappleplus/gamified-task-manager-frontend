a7b911274e1c202873317d66d78fc1eba002448e
sweetappleplus <sweetappleplus@gmail.com>
2026-02-10 19:12:05 +0200

    feat(reducer): add system setting reducer to root reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { authReducer } from "features/auth";
 import { taskCategoryReducer } from "features/task-category";
+import { systemSettingReducer } from "features/system-setting";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   taskCategory: taskCategoryReducer,
+  systemSetting: systemSettingReducer,
 });
 
 export default rootReducer;
