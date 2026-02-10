935d3e5f206b7b328464e53916936f5f6733b625
sweetappleplus <sweetappleplus@gmail.com>
2026-02-10 17:42:45 +0200

    feat(reducer): add task category reducer to root reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { authReducer } from "features/auth";
+import { taskCategoryReducer } from "features/task-category";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  taskCategory: taskCategoryReducer,
 });
 
 export default rootReducer;
