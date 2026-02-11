baf76d43160aeabbfbe8df38ce6f9b59c172c0cb
sweetappleplus <sweetappleplus@gmail.com>
2026-02-11 16:12:28 +0200

    feat(api): add additional task and user API endpoints

--- src/consts/api.ts
+++ src/consts/api.ts
@@ -1,4 +1,5 @@
 export const API_URL = process.env.REACT_APP_API_URL;
+export const BACKEND_URL = API_URL?.replace(/\/api$/, "") ?? "";
 
 export const API_URL_AUTH_SEND_OTP = "/auth/send-otp";
 export const API_URL_AUTH_VERIFY_OTP = "/auth/verify-otp";
@@ -15,6 +16,16 @@ export const API_URL_SYSTEM_SETTINGS_BY_KEY = "/system-settings/:key";
 
 export const API_URL_LEVEL_CONFIGS = "/level-configs";
 export const API_URL_LEVEL_CONFIGS_BY_ID = "/level-configs/:id";
+
+export const API_URL_TASKS = "/tasks";
+export const API_URL_TASKS_BY_ID = "/tasks/:id";
+export const API_URL_TASKS_ASSIGN = "/tasks/:id/assign";
+export const API_URL_TASKS_REVIEW = "/tasks/:id/review";
+export const API_URL_TASKS_MARK_PAID = "/tasks/:id/mark-paid";
+export const API_URL_TASKS_CANCEL = "/tasks/:id/cancel";
+export const API_URL_TASKS_FILES = "/tasks/:id/files";
+
+export const API_URL_USERS = "/users";
 
 export const PUBLIC_ENDPOINTS = [
   API_URL_AUTH_SEND_OTP,
