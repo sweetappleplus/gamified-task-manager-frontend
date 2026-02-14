5abf84e291b31f6102b4a3bd08edd601ec86c1f4
sweetappleplus <sweetappleplus@gmail.com>
2026-02-14 15:46:30 +0200

    feat(app): wrap AppRouter with ModalProvider for modal support

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./libs/theme";
-import { ToastProvider } from "./hooks";
+import { ModalProvider, ToastProvider } from "./hooks";
 import AppRouter from "./routes/AppRouter";
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <ToastProvider>
-      <AppRouter />
+      <ModalProvider>
+        <AppRouter />
+      </ModalProvider>
     </ToastProvider>
   </ThemeProvider>
 );
