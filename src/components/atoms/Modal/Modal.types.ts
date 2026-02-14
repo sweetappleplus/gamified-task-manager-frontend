807b68c260cf294aa1abee315afce150aeacd190
sweetappleplus <sweetappleplus@gmail.com>
2026-02-14 15:48:00 +0200

    feat(components): add ModalProps interface for modal component

---
+++ src/components/atoms/Modal/Modal.types.ts
@@ -0,0 +1,24 @@
+import React from "react";
+
+export interface ModalProps {
+  /**
+   * Whether the modal is open
+   */
+  open: boolean;
+  /**
+   * Callback fired when the modal is closed
+   */
+  onClose: () => void;
+  /**
+   * The title displayed in the modal header
+   */
+  title: string;
+  /**
+   * The content rendered in the modal body
+   */
+  body: React.ReactNode;
+  /**
+   * Optional content rendered in the sticky footer
+   */
+  footer?: React.ReactNode;
+}
