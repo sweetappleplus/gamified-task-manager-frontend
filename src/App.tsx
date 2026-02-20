661646f166f641ea2a491c78bfe7a1070445ff8f
sweetappleplus <sweetappleplus@gmail.com>
2026-02-20 14:15:09 +0200

    refactor(app): remove ModalProvider from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./libs/theme";
-import { ModalProvider, ToastProvider } from "./hooks";
+import { ToastProvider } from "./hooks";
 import AppRouter from "./routes/AppRouter";
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <ToastProvider>
-      <ModalProvider>
-        <AppRouter />
-      </ModalProvider>
+      <AppRouter />
     </ToastProvider>
   </ThemeProvider>
 );
